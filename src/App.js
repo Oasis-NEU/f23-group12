13f538745b3b5fa827301c017f2e6f78d45bf096
abbyelbaum <abby.elbaum@gmail.com>
2023-10-01 13:56:29 -0400

    Started App

--- src/App.js
+++ src/App.js
@@ -3,22 +3,35 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <label>
+      <center>
+        <header>
+          <big>
+          Upload Your Own Event!!
+          </big>
+          </header>
+      Name of the Event: <input name="name" />
+      <br></br>
+      Location: <input location="location" />
+      <br></br>
+
+      <label for="event-type">Choose Your Event Type:</label> 
+    <select name="event-type" id="event-type"> 
+        <option value="club">Club</option> 
+        <option value="frat">Frat</option> 
+        <option value="food-and-drink">Food and Drink</option> 
+        <option value="festival">Festival</option> 
+        <option value="concert">Concert</option> 
+        <option value="other">Other</option> 
+    </select>
+  
+<br></br>
+
+
+
+      Description: <textarea description="description" cols="50" rows="10"></textarea>
+      </center>
+    </label>
   );
 }
 
