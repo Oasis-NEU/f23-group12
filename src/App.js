c7547023b6acfebc8229e19439207cb18ab03fd9
abbyelbaum <abby.elbaum@gmail.com>
2023-10-15 13:57:12 -0400

    router!

--- src/App.js
+++ src/App.js
@@ -1,38 +1,28 @@
 
 import './App.css';
+import Home from './Home';
+
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <a href="/createEvent">create</a>,//<Home />,
+  },
+  {
+    path: "/createEvent",
+    element: <p>Create Event</p>
+  }
+]);
 
 function App() {
-  return (
-    <label>
-      <center>
-        <header>
-          <big>
-          Upload Your Own Event!!
-          </big>
-          </header>
-      Name of the Event: <input name="name" />
-      <br></br>
-      Location: <input location="location" />
-      <br></br>
 
-      <label for="event-type">Choose Your Event Type:</label> 
-    <select name="event-type" id="event-type"> 
-        <option value="club">Club</option> 
-        <option value="frat">Frat</option> 
-        <option value="food-and-drink">Food and Drink</option> 
-        <option value="festival">Festival</option> 
-        <option value="concert">Concert</option> 
-        <option value="other">Other</option> 
-    </select>
-  
-<br></br>
-
-
-
-      Description: <textarea description="description" cols="50" rows="10"></textarea>
-      </center>
-    </label>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ src/mainPage.js
@@ -0,0 +1,16 @@
+function mainPage() {
+return (
+    <label>
+      <center>
+        <header>
+          <big>
+          Main Page
+          </big>
+          </header>
+      </center>
+      
+    </label>
+  );
+}
+
+export default mainPage;
