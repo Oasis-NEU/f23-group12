f258bf18e1d6fbf2ae195c2511ada81d28ce4124
abbyelbaum <abby.elbaum@gmail.com>
2023-10-15 13:29:56 -0400

    supabase setup

---
+++ src/supabase.js
@@ -0,0 +1,6 @@
+import { createClient } from "@supabase/supabase-js"
+
+const supabaseURL = "https://bhhhuiwpokecohcaplyb.supabase.co"
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJoaGh1aXdwb2tlY29oY2FwbHliIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTczODY0ODIsImV4cCI6MjAxMjk2MjQ4Mn0.9RQGguj5F5H51m6ntCx4iM56hKCagKtcj0aUWyMpJXM"
+
+export const  supabase = createClient(supabaseURL, supabaseKey)
