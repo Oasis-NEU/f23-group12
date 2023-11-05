5b993f0b6a57299c18653b8e0e83fac934ecc91b
abbyelbaum <abby.elbaum@gmail.com>
2023-11-05 13:13:26 -0500

    made form

--- src/Aboutpage.js
+++ src/Aboutpage.js
@@ -1,5 +1,24 @@
+import { useState } from "react";
+
 function AboutPage() {
+
+/* constants and functions to set the names of the variables on input */
+/* these get used in the <input> labels on the form so the page knows to take the values
+and set the appropriate variables. */
+const [name, setName] = useState("")
+const [location, setLocation] = useState("")
+const [eventType, setType] = useState("")
+const [textArea, setTextArea] = useState("")
+
+/* function to handle the event of submitting the form */
+const handleSubmit = (event) => {
+  event.preventDefault();
+  alert(`The name you entered was: ${name}. The location you entered was: ${location}. The type of event you selected was ${eventType}. The description of the event is ${textArea}.`);
+}
+
     return (
+      /* <form> has an onSubmit tag to link it to the handleSubmit function */
+      <form onSubmit = {handleSubmit}>
       <label>
         <center>
           <header>
@@ -7,13 +26,24 @@ function AboutPage() {
             Upload Your Own Event!!
             </big>
             </header>
-        Name of the Event: <input name="name" />
+        Name of the Event:
+        <input type = "text"
+          value = {name}
+          onChange={(e) => setName(e.target.value)} />
         <br></br>
-        Location: <input location="location" />
+
+        Location:
+        <input type = "text"
+          value = {location}
+          onChange={(e) => setLocation(e.target.value)} />
         <br></br>
   
-        <label for="event-type">Choose Your Event Type:</label> 
-      <select name="event-type" id="event-type"> 
+         <label for="event-type">Choose Your Event Type:</label> 
+      <select type="text"
+             value={eventType}
+             onChange={(e) => setType(e.target.value)}> 
+             defaultValue="club"
+          <option value=""></option> 
           <option value="club">Club</option> 
           <option value="frat">Frat</option> 
           <option value="food-and-drink">Food and Drink</option> 
@@ -21,16 +51,16 @@ function AboutPage() {
           <option value="concert">Concert</option> 
           <option value="other">Other</option> 
       </select>
-    
-  <br></br>
-  
-  
-  
-        Description: <textarea description="description" cols="50" rows="10"></textarea>
+      <br></br>
+        Description: <textarea description="description" cols="50" rows="10"
+        value={textArea}
+        onChange={(e) => setTextArea(e.target.value)}
+        ></textarea>
         <br></br>
-        <button>Create!</button>
+        <button type="submit">Create!</button>
         </center>
       </label>
+      </form>
     );
   }
 
